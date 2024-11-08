07b8041d7753f52350cb2a20dc4a0b620fe1faf2
neptune-dev-23 <anthony@deeperedge.com>
2024-11-08 16:39:59 -0500

    Moved header to separate file, now loads via JS

---
+++ public/load_header.js
@@ -0,0 +1,11 @@
+function loadHTML(elementId, url) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById(elementId).innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+loadHTML("header", "header.html");
